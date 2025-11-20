05e33d412c975102bd2c3588c6f04caa74daccec
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-11-20 13:38:24 +0700

    Init answer-service

---
+++ answer-service/src/test/java/io/github/tdminhnhat/project/quizzick/service/AnswerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tdminhnhat.project.quizzick.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
