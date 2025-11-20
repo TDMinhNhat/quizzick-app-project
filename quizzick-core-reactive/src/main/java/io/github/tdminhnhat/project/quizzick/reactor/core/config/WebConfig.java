7298b9dd16be936c152fa0fc3e4e975e5033af59
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-11-20 13:30:55 +0700

    Update configure

--- quizzick-core-reactive/src/main/java/io/github/tdminhnhat/project/quizzick/reactor/core/config/WebConfig.java
+++ quizzick-core-reactive/src/main/java/io/github/tdminhnhat/project/quizzick/reactor/core/config/WebConfig.java
@@ -1,11 +1,15 @@
 package io.github.tdminhnhat.project.quizzick.reactor.core.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class CorsConfig implements WebFluxConfigurer {
+@EnableWebFlux
+@EnableSpringDataWebSupport
+public class WebConfig implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
