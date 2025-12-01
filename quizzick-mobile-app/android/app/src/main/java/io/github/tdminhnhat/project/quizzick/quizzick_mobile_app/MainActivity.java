c71f845e0e241c1e19b054079e088b5b13fca188
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-12-01 08:48:34 +0700

    Init project

---
+++ quizzick-mobile-app/android/app/src/main/java/io/github/tdminhnhat/project/quizzick/quizzick_mobile_app/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.tdminhnhat.project.quizzick.quizzick_mobile_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ quizzick-mobile-app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:quizzick_mobile_app/page/LoginPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      routes: {
+        "/login": (context) => const LoginPage()
+      },
+      home: const LoginPage()
+    );
+  }
+}
